876f0b07ee752383977d38d432e4b5a3371cd93d
vvishwes <32951470+vvishwes@users.noreply.github.com>
2017-10-20 14:55:16 +0530

    dbconnect file to local

---
+++ db_connect.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# USAGE: db_connect.sh <dbname> <dbhome>
+databasename=$1
+dbnm=`echo $databasename | tr A-Z a-z`
+dbhome=$2
+echo "connecting to  database $dbnm... "
+export ORACLE_HOME=$dbhome
+export ORACLE_SID=${dbnm}1
+su oracle -c "$dbhome/bin/sqlplus -s / as sysdba <<!
+spool something
+select name from v\\\$database;
+spool off;
+exit
+!
+"
